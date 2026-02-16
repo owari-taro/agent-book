d3cfad314e26d3c195e154570a8cfad005993d89
owari-taro <58625526+owari-taro@users.noreply.github.com>
2026-02-16 19:27:07 +0900

    fix model id

--- chapter2/1_converse.py
+++ chapter2/1_converse.py
@@ -3,14 +3,14 @@ import boto3
 from dotenv import load_dotenv
 
 # .envファイルから環境変数を読み込む
-load_dotenv()
+#load_dotenv()
 
 # Bedrock呼び出し用のAPIクライアントを作成
-client = boto3.client("bedrock-runtime")
+client = boto3.client("bedrock-runtime",region_name="us-west-2") # Bedrockが利用可能なリージョンを指定ss
 
 # Converse APIを実行
 response = client.converse(
-    modelId="us.anthropic.claude-3-7-sonnet-20250219-v1:0", # モデルID
+    modelId="global.anthropic.claude-sonnet-4-20250514-v1:0", # モデルID
     messages=[{
         "role": "user",
         "content": [{
